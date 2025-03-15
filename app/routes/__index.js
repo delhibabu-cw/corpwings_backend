30cdccdf5d41328b5e2be353429ceed84be604f1
Delhibabu7172 <rldb143@gmail.com>
2025-03-15 20:17:17 +0530

    chandru bro finall last commit

--- app/routes/__index.js
+++ app/routes/__index.js
@@ -1,26 +1,21 @@
-/* eslint-disable no-undef */
-const { express, fs } = require('../services/imports');
-
+const express = require("express");
 const router = express.Router();
-const middleware = require('../middlewares');
+const middleware = require("../middlewares");
 
-fs.readdirSync(__dirname)
-  .filter((file) => file.indexOf('.') !== 0 && file !== '__index.js' && file !== '__common.js')
-  .forEach((file) => {
-    console.info(`Loading file ${file}`);
-    if (file.slice(-3) === '.js') {
-      const routesFile = require(`${__dirname}/${file}`);
-      // console.log(routesFile);
-      
-      if (file === 'auth.js') {
-        router.use('/', middleware.checkSetToken(), routesFile);
-      } else {
-        router.use('/', middleware.checkSetToken(), routesFile);
-      }
-    } else if (fs.lstatSync(`${__dirname}/${file}`).isDirectory() && fs.existsSync(`${__dirname}/${file}/__index.js`)) {
-      const indexFile = require(`${__dirname}/${file}/__index.js`);
-      router.use(indexFile.routes(), indexFile.allowedMethods());
-    }
-  });
+// Import each route manually
+const authRoutes = require("./auth");
+const userRoutes = require("./user");
+const careersRoutes = require("./carrers");
+const jobApplicationRoutes = require("./jobApplication");
+const roleRoutes = require("./role");
+const uploadRoutes = require("./upload");
+
+// Apply middleware and define routes
+router.use("/auth", middleware.checkSetToken(), authRoutes);
+router.use("/user", middleware.checkSetToken(), userRoutes);
+router.use("/careers", middleware.checkSetToken(), careersRoutes);
+router.use("/jobApplication", middleware.checkSetToken(), jobApplicationRoutes);
+router.use("/role", middleware.checkSetToken(), roleRoutes);
+router.use("/upload", middleware.checkSetToken(), uploadRoutes);
 
 module.exports = router;
