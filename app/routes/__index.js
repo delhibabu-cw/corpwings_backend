776d7ce83d06e36098563c3edb0a242ccd36dec1
Delhibabu7172 <rldb143@gmail.com>
2025-03-16 12:36:12 +0530

    rrrrr comit

--- app/routes/__index.js
+++ app/routes/__index.js
@@ -1,19 +1,39 @@
-const express = require('express')
-const fs = require('fs')
-const path = require('path')
-const middleWare = require('../middlewares')
-const router = express.Router()
+/* eslint-disable no-undef */
+const { express, fs, path } = require('../services/imports');
 
+const router = express.Router();
+const middleware = require('../middlewares');
+
+console.log(__dirname);
 
 fs.readdirSync(__dirname).filter((file) =>
-    file.indexOf('.') !== 0 &&
-    file !== '__index.js'
+  file.indexOf('.') !== 0 &&
+  file !== '__index.js'
 ).forEach((file) => {
 
-    const newRoot = path.join(__dirname, file)
-    const routesFile = require(newRoot)
-    router.use('/', middleWare.isAuthorized(), routesFile)
+  const newRoot = path.join(__dirname, file)
+  const routesFile = require(newRoot)
+  router.use('/', middleware.isAuthorized(), routesFile)
 
 })
+module.exports = router;
 
-module.exports = router
+// const express = require('express')
+// const fs = require('fs')
+// const path = require('path')
+// const middleWare = require('../middleWares')
+// const router = express.Router()
+
+
+// fs.readdirSync(__dirname).filter((file) =>
+//     file.indexOf('.') !== 0 &&
+//     file !== '__index.js'
+// ).forEach((file) => {
+
+//     const newRoot = path.join(__dirname, file)
+//     const routesFile = require(newRoot)
+//     router.use('/', middleWare.isAuthorized(), routesFile)
+
+// })
+
+// module.exports = router
